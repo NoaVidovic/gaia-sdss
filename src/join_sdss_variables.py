c34b03d809e99df6c42f36b14215ae30901c7910
DenisHudincec <odvojeniacc@gmail.com>
2023-07-23 14:19:54 +0200

    * Added csv cleanup and fixed join

--- src/join_sdss_variables.py
+++ src/join_sdss_variables.py
@@ -1,12 +1,8 @@
 import os
 import pandas as pd
 import numpy as np
-import sys
+from util import get_script_path
 from sklearn.neighbors import BallTree
-
-def get_script_path():
-    return os.path.dirname(os.path.realpath(sys.argv[0]))
-
 
 data_path = os.path.join(get_script_path(), '..', 'data')
 
@@ -14,31 +10,36 @@ gaia_sdss_joined = pd.read_csv(os.path.join(data_path, 'gaia_sdss_joined.csv'))
 variables = pd.read_csv(os.path.join(data_path, 'variables.csv'))
 
 # Convert degrees to radians for calculations
-gaia_sdss_joined[['photoRa', 'photoDec']] = np.radians(gaia_sdss_joined[['photoRa', 'photoDec']])
 variables[['ra', 'dec']] = np.radians(variables[['ra', 'dec']])
-
 
 def find_nearby_stars(df1, df2):
     # Create a BallTree for efficient nearest neighbor search
     tree = BallTree(df2[['ra', 'dec']], metric='haversine')
 
     # Query the tree for neighbors within given radius
-    dist = tree.query_radius(df1[['photoRa', 'photoDec']], r=0.1/3600)
+    ind = tree.query_radius(df1[['photoRa', 'photoDec']], r=np.radians(0.1/3600))
 
+    # Initialize the variablesID column with empty values
     df1['variablesID'] = pd.NA
+
+    # Initialize an empty DataFrame for multiple matches
     variable_multiples = pd.DataFrame(columns=['source_id', 'variablesID'])
 
-    for i, matches in enumerate(dist):
+    # Handle each case (no matches, one match, multiple matches) separately
+    for i, matches in enumerate(ind):
         if len(matches) == 1:  # One match
             df1.loc[i, 'variablesID'] = df2.loc[matches[0], 'ID']
         elif len(matches) > 1:  # Multiple matches
             new_rows = [{'source_id': df1.loc[i, 'source_id'], 'variablesID': df2.loc[match, 'ID']} for match in matches]
             variable_multiples = pd.concat([variable_multiples, pd.DataFrame(new_rows)], ignore_index=True)
 
+    # Write the result to a new CSV file
     df1.to_csv(os.path.join(data_path, 'gaia_sdss_joined_with_variablesID.csv'), index=False)
+
+    # Write multiple matches to a new CSV file
     variable_multiples.to_csv(os.path.join(data_path, 'variable_multiples.csv'), index=False)
 
     return df1, variable_multiples
 
-
-find_nearby_stars(gaia_sdss_joined, variables)
+# Call the function
+result, multiples = find_nearby_stars(gaia_sdss_joined, variables)
